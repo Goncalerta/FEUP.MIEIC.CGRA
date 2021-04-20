3fa84fb316aeb93a7c2fbfedfe0c778a48f6d22f
Pedro Correia <up201905348@fe.up.pt>
2021-04-20 17:58:43 +0100

    Partial eye

--- project/MyFish.js
+++ project/MyFish.js
@@ -20,9 +20,16 @@ export class MyFish extends CGFobject {
 		this.bodyAppearance.setAmbient(0.3, 0.3, 0.3, 1);
 		this.bodyAppearance.setDiffuse(0.7, 0.7, 0.7, 1);
 		this.bodyAppearance.setSpecular(0.0, 0.0, 0.0, 1);
-		this.bodyAppearance.setShininess(120);
+		this.bodyAppearance.setShininess(1);
         this.bodyAppearance.setTexture(new CGFtexture(this.scene, 'images/fishbody_texture.jfif'));
 
+
+        this.eyeAppearance = new CGFappearance(this.scene);
+		this.eyeAppearance.setAmbient(0.3, 0.3, 0.3, 1);
+		this.eyeAppearance.setDiffuse(0.7, 0.7, 0.7, 1);
+		this.eyeAppearance.setSpecular(0.0, 0.0, 0.0, 1);
+		this.eyeAppearance.setShininess(1);
+        this.eyeAppearance.setTexture(new CGFtexture(this.scene, 'images/fisheye.png'));
 
         this.eye = new MySphere(scene, 20, 20);
     }
@@ -36,12 +43,17 @@ export class MyFish extends CGFobject {
     }
 
     display() {
-        // this.scene.pushMatrix();
+        this.scene.pushMatrix();
 
-        // this.scene
-        // this.eye.display();
+        
+        this.scene.translate(0.4, 0.5, 1);
+        this.scene.scale(0.18, 0.18, 0.18);
+        this.scene.rotate(1, 0, 0);
 
-        // this.scene.popMatrix();
+        this.eyeAppearance.apply();
+        this.eye.display();
+
+        this.scene.popMatrix();
 
         this.scene.setActiveShader(this.bodyShader);
         this.bodyAppearance.apply();
